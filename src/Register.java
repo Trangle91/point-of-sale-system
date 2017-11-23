2276a96771ee9e8b9b3d04fe868b31725833b13d
Cyndy Pooley <cynthia.pooley@gmail.com>
2017-11-22 23:22:11 -0600

    initial commit

---
+++ src/Register.java
@@ -0,0 +1,55 @@
+
+public class Register {
+	private String logInTime;
+	private String logOutTime;
+	private double salesAmount;
+	private String employeeUser;
+	
+	public Register(){
+		
+	}
+	
+	public void returnItem(Item item){
+		
+	}
+	
+	public void sellItem(Item item){
+		
+	}
+	
+	public void cancelOrder(){
+		
+	}
+
+	public String getLogInTime() {
+		return logInTime;
+	}
+
+	public void setLogInTime(String logInTime) {
+		this.logInTime = logInTime;
+	}
+
+	public String getLogOutTime() {
+		return logOutTime;
+	}
+
+	public void setLogOutTime(String logOutTime) {
+		this.logOutTime = logOutTime;
+	}
+
+	public double getSalesAmount() {
+		return salesAmount;
+	}
+
+	public void setSalesAmount(double salesAmount) {
+		this.salesAmount = salesAmount;
+	}
+
+	public String getEmployeeUser() {
+		return employeeUser;
+	}
+
+	public void setEmployeeUser(String employeeUser) {
+		this.employeeUser = employeeUser;
+	}
+}
