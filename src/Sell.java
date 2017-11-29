1a68254f5a584398bbe8066039cf436decf41e9a
Cyndy Pooley <cynthia.pooley@gmail.com>
2017-11-28 22:05:59 -0500

    initial sell gui

---
+++ src/Sell.java
@@ -0,0 +1,148 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JScrollPane;
+import javax.swing.JScrollBar;
+import javax.swing.UIManager;
+
+public class Sell extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textFieldItem;
+	private JTextField textFieldQuantity;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Sell frame = new Sell();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Sell() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		
+		JLabel labelSaleTitle = new JLabel("SALE");
+		labelSaleTitle.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		labelSaleTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JLabel labelItem = new JLabel("Item");
+		
+		JLabel labelQuantity = new JLabel("Quantity");
+		
+		textFieldItem = new JTextField();
+		textFieldItem.setColumns(10);
+		
+		textFieldQuantity = new JTextField();
+		textFieldQuantity.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+		JScrollBar scrollBar = new JScrollBar();
+		
+		JButton btnAddToList = new JButton("Add To List");
+		
+		JButton btnAddAnotherItem = new JButton("Add Another Item");
+		
+		JButton btnFinishPay = new JButton("Finish & Pay");
+		
+		JButton btnCancel = new JButton("Cancel");
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(173)
+					.addComponent(labelSaleTitle)
+					.addContainerGap(220, Short.MAX_VALUE))
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(49)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(btnFinishPay)
+							.addContainerGap())
+						.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+							.addGroup(gl_panel.createSequentialGroup()
+								.addComponent(labelItem)
+								.addGap(18)
+								.addComponent(textFieldItem, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED, 73, Short.MAX_VALUE)
+								.addComponent(labelQuantity)
+								.addPreferredGap(ComponentPlacement.UNRELATED)
+								.addComponent(textFieldQuantity, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addGap(38))
+							.addGroup(gl_panel.createSequentialGroup()
+								.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING, false)
+									.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
+										.addComponent(btnAddToList)
+										.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+										.addComponent(btnAddAnotherItem))
+									.addComponent(scrollPane, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 340, GroupLayout.PREFERRED_SIZE)
+									.addComponent(btnCancel))
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addComponent(scrollBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addContainerGap()))))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(labelSaleTitle)
+					.addGap(18)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(labelItem)
+						.addComponent(labelQuantity)
+						.addComponent(textFieldItem, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textFieldQuantity, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING, false)
+						.addComponent(scrollBar, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(scrollPane, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 97, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnAddToList)
+						.addComponent(btnAddAnotherItem))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnFinishPay)
+						.addComponent(btnCancel))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		panel.setLayout(gl_panel);
+	}
+}
